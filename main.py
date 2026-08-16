1d038b59b814ffb4954cbb6f26d32fb87bd24dda
Kumar Rohit <93335681+k-Rohit@users.noreply.github.com>
2026-08-16 17:24:57 +0530

    Implement FastAPI application with posts endpoint and add posts data snippet

--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from fastapi-tutorial!")
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+posts: list[dict] = [
+    {
+        "id" : 1,
+        "author": "Rohit",
+        "title": "FastAPI is Awsome",
+        "content" : "The is a ASGI web framework",
+        "date_posted" : "August 16, 2026"
+    },
+
+    {
+        "id" : 2,
+        "author": "Shweta",
+        "title": "Databricks is Awsome",
+        "content" : "The is a data analytics and processing platform",
+        "date_posted" : "August 17, 2026"
+    }
+]
+
+@app.get("/",tags=["Home"], response_class=HTMLResponse, include_in_schema=False)
+@app.get("/posts", response_class=HTMLResponse, include_in_schema=False) # here we have stacked the decorators to make the same function respond to two different routes
+
+# include_in_schema=False is used to hide the route from the documentation page
 
 
-if __name__ == "__main__":
-    main()
+def home():
+    return f"<h1>{posts[0]['title']}</h1>"
+
+@app.get("/api/posts", response_class=HTMLResponse, tags=["Posts"])
+def get_posts():
+    return posts
+
+
+
