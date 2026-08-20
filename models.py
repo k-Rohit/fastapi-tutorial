f6c182330991ad188a49765708af711b747c12d2
Kumar Rohit <93335681+k-Rohit@users.noreply.github.com>
2026-08-20 21:32:37 +0530

    fix: add newline at end of file in models.py

--- models.py
+++ models.py
@@ -4,3 +4,8 @@ from sqlalchemy import DateTime, ForeignKey, Integer, String, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database import Base
+
+class User(Base):
+    
+    __tablename__ = "users"
+    
